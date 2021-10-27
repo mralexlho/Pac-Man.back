b9d81e89d444babf15bdc72077babbba201264a3
mralexlho <43863102+mralexlho@users.noreply.github.com>
2021-10-27 15:47:10 +0200

    add app

---
+++ src/index.ts
@@ -0,0 +1,81 @@
+import express from "express";
+
+const PORT = process.env.PORT || 3000;
+const socketIO = require('socket.io');
+
+const app = express()
+	.use((req, res) => res.sendFile('/client/index.html', { root: __dirname }))
+	.listen(PORT, () => console.log(`Listening on https://swapmanback.herokuapp.com`));
+
+const io = socketIO(app, {
+	cors: {
+		origin: "*",
+		methods: ["GET", "POST"],
+		transports: ['websocket', 'polling'],
+		credentials : true
+	},
+	allowEIO3: true
+});
+
+let rooms: any = {}
+
+io.on("connection", function (socket: any) {
+	socket.on("createRoom", function (room: string, name: string) {
+		socket.join(room);
+		rooms[room] = { user1: name, user2: "" }
+		console.log(`${name} create room : ${room}`);
+		io.in(room).emit('user', rooms[room])
+	});
+	socket.on("joinRoom", function (room: string, name: string) {
+
+		if (rooms[room]) {
+			if (rooms[room].user2 != "") {
+				socket.emit("roomFull")
+				return
+			}
+			socket.join(room);
+			rooms[room].user2 = name
+			console.log(`${name} join room : ${room}`);
+			io.in(room).emit('user', rooms[room])
+		} else {
+			socket.join(room);
+			rooms[room] = { user1: name, user2: "" }
+			console.log(`${name} create room : ${room}`);
+			io.in(room).emit('user', rooms[room])
+		}
+	});
+	socket.on("startGame", function (room: string) {
+		io.in(room).emit('startGame')
+	});
+	socket.on("pacmanMove", function (pacman: any, room: string) {
+		socket.to(room).emit("pacmanMove", pacman)
+	});
+	socket.on("Player2Move", function (pacman: any, room: string) {
+		socket.to(room).emit("Player2Move", pacman)
+	});
+	socket.on("changeMap", function (room: any, map: string) {
+		socket.to(room).emit("changeMap", map)
+	});
+	socket.on("deco", function (room: any) {
+		socket.to(room).emit("deco")
+		delete rooms[room]
+	});
+
+
+
+	socket.on("SenderUp", function (room: string, type: string) {
+		socket.to(room).emit("Up", type)
+	})
+	socket.on("SenderDown", function (room: string, type: string) {
+		socket.to(room).emit("Down", type)
+	})
+	socket.on("SenderLeft", function (room: string, type: string) {
+		socket.to(room).emit("Left", type)
+	})
+	socket.on("SenderRight", function (room: string, type: string) {
+		socket.to(room).emit("Right", type)
+	})
+	socket.on("SenderClear", function (room: string, type: string) {
+		socket.to(room).emit("Clear", type)
+	})
+});
